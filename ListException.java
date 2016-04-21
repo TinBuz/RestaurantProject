934042ce246ea4517c4110945d3b5a455ccda8a8
TinBuz <TinBuz@users.noreply.github.com>
2016-04-21 20:34:05 -0300

    Added files via upload

---
+++ ListException.java
@@ -0,0 +1,17 @@
+/* Purpose: Data Structure and Algorithms Lab 2 Problem 1
+ * Status: Complete and thoroughly tested
+ * Last update: 02/03/2016
+ * Submitted:  02/04/2016
+ * Comment: List Exception
+ * @author: Tin Buzancic
+ * @version: 2016.04.02
+ */
+
+public class ListException extends RuntimeException
+{
+    public ListException(String s)
+    {
+        super(s);
+    }  // end constructor
+}  // end ListException
+
---
+++ ListIndexOutOfBoundsException.java
@@ -0,0 +1,18 @@
+/* Purpose: Data Structure and Algorithms Lab 2 Problem 1
+ * Status: Complete and thoroughly tested
+ * Last update: 02/03/2016
+ * Submitted:  02/04/2016
+ * Comment: Out of bounds exception
+ * @author: Tin Buzancic
+ * @version: 2016.04.02
+ */
+
+public class ListIndexOutOfBoundsException
+    extends IndexOutOfBoundsException
+{
+    public ListIndexOutOfBoundsException(String s)
+    {
+        super(s);
+    }  // end constructor
+}  // end ListIndexOutOfBoundsException
+
