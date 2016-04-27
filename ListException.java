4eb165475b4964078ccedebb7e71ea425193fef7
TinBuz <TinBuz@users.noreply.github.com>
2016-04-26 23:43:38 -0300

    Update ListException.java

--- ListException.java
+++ ListException.java
@@ -1,17 +1,22 @@
-/* Purpose: Data Structure and Algorithms Lab 2 Problem 1
+/**
+ * Purpose: DSA Final
  * Status: Complete and thoroughly tested
- * Last update: 02/03/2016
- * Submitted:  02/04/2016
- * Comment: List Exception
+ * Last update: 4/26/2016
+ * Submitted:  4/28/2016
+ * Comment: Exception for List ADT
  * @author: Tin Buzancic
  * @version: 2016.04.02
  */
 
 public class ListException extends RuntimeException
 {
-    public ListException(String s)
-    {
-        super(s);
-    }  // end constructor
+	/**
+	 * Constructor for Exception
+	 * @param s
+	 * Default String
+	 */
+	public ListException(String s)
+	{
+		super(s);
+	}  // end constructor
 }  // end ListException
-
