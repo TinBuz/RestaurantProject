3f18eca8d412ef79ed10fbf176c1e84bac20adaf
TinBuz <TinBuz@users.noreply.github.com>
2016-04-26 23:44:20 -0300

    Update ListIndexOutOfBoundsException.java

--- ListIndexOutOfBoundsException.java
+++ ListIndexOutOfBoundsException.java
@@ -1,18 +1,23 @@
-/* Purpose: Data Structure and Algorithms Lab 2 Problem 1
- * Status: Complete and thoroughly tested
- * Last update: 02/03/2016
- * Submitted:  02/04/2016
- * Comment: Out of bounds exception
+/** 
+ * Purpose: DSA Final
+ * Status: Ready for Running
+ * Last update: 04/26/2016
+ * Submitted:  04/28/2016
+ * Comment: Index out of Bounds Exception for List ADT
  * @author: Tin Buzancic
  * @version: 2016.04.02
  */
 
 public class ListIndexOutOfBoundsException
-    extends IndexOutOfBoundsException
+extends IndexOutOfBoundsException
 {
-    public ListIndexOutOfBoundsException(String s)
-    {
-        super(s);
-    }  // end constructor
+	/**
+	 * Constructor for Index Out Of Bounds Exception
+	 * @param s
+	 * Default Case
+	 */
+	public ListIndexOutOfBoundsException(String s)
+	{
+		super(s);
+	}  // end constructor
 }  // end ListIndexOutOfBoundsException
-
