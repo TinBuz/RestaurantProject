7818156e87421e9c3bd016c45e27a450117730fa
TinBuz <TinBuz@users.noreply.github.com>
2016-04-26 23:44:47 -0300

    Update ListInterface.java

--- ListInterface.java
+++ ListInterface.java
@@ -1,15 +1,61 @@
-// ********************************************************
-// Interface ListInterface for the ADT list.
-// *********************************************************
+/**
+ * Purpose: DSA Final
+ * Status: Complete and thoroughly tested
+ * Last update: 4/26/2016
+ * Submitted:  4/28/2016
+ * Comment: Interface for List ADT
+ * @author: Tin Buzancic
+ * @version: 2016.04.02
+ */
+
 public interface ListInterface 
 {
-  boolean isEmpty();
-  int size();
-  void add(int index, Object item) 
-                  throws ListIndexOutOfBoundsException;
-  Object get(int index) 
-                    throws ListIndexOutOfBoundsException;
-  void remove(int index) 
-                     throws ListIndexOutOfBoundsException;
-  void removeAll();
+	/**
+	 * Determines whether a List is empty.
+	 * @return
+	 * boolean
+	 */
+	boolean isEmpty();
+
+	/**
+	 * Give us the size number of the List
+	 * @return
+	 * Integer Size
+	 */
+	int size();
+
+	/**
+	 * Adds an item to the list at a designated index
+	 * @param index
+	 * Location in List
+	 * @param item
+	 * object you wish to add
+	 * @throws ListIndexOutOfBoundsException
+	 */
+	void add(int index, Object item) 
+			throws ListIndexOutOfBoundsException;
+
+	/**
+	 * Retrieves an item at a designated index
+	 * @param index
+	 * Index of item you wish to get
+	 * @return
+	 * @throws ListIndexOutOfBoundsException
+	 */
+	Object get(int index) 
+			throws ListIndexOutOfBoundsException;
+
+	/**
+	 * Removes and item at a designated index
+	 * @param index
+	 * Index of item you wish to delete
+	 * @throws ListIndexOutOfBoundsException
+	 */
+	void remove(int index) 
+			throws ListIndexOutOfBoundsException;
+
+	/**
+	 * Removes all items from the List.
+	 */
+	void removeAll();
 }  // end ListInterface
